9f8319aa46331dc5a6fc3d63345c4de58c927174
Ash Carter <acarter010204@gmail.com>
2026-04-14 00:39:06 +1000

    Lesson 5, The magic of perspective

--- src/stars3D.rs
+++ src/stars3D.rs
@@ -5,6 +5,7 @@ pub struct Stars3D {
     spread: f32,
     speed: f32,
     num_stars: usize,
+    half_tan_fov: f32,
     stars_x: Vec<f32>,
     stars_y: Vec<f32>,
     stars_z: Vec<f32>,
@@ -16,6 +17,7 @@ impl Stars3D {
             spread,
             speed,
             num_stars,
+            half_tan_fov : (70.0 as f32 /2.0).to_radians().tan(),
             stars_x: vec![0.0; num_stars],
             stars_y: vec![0.0; num_stars],
             stars_z: vec![0.0; num_stars],
@@ -47,8 +49,8 @@ impl Stars3D {
                 self.init_star(index);
             }
 
-            let x = ((self.stars_x[index]/self.stars_z[index])  * half_width + half_width) as i64;
-            let y = ((self.stars_y[index]/self.stars_z[index]) * half_height + half_height) as i64;
+            let x = ((self.stars_x[index]/(self.stars_z[index] * self.half_tan_fov))  * half_width + half_width) as i64;
+            let y = ((self.stars_y[index]/(self.stars_z[index] * self.half_tan_fov)) * half_height + half_height) as i64;
 
             if (x < 0 || x >= target.width() as i64) || (y < 0 || y >= target.height() as i64) {
                 self.init_star(index);
